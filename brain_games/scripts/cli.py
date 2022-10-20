ff5d37b836abcbcadf40f5216d231080b0449c79
OlegKarush <karusholeg@mail.ru>
2022-10-20 19:11:59 +0300

    add cli.py

--- brain_games/scripts/cli.py
+++ brain_games/scripts/cli.py
@@ -1,6 +1,8 @@
+#!/usr/bin/env python3
 
 
 def welcome_user():
     import prompt
     get_name = prompt.string("May I have your name? ")
     return print("Hello, " + get_name + "!")
+welcome_user()
